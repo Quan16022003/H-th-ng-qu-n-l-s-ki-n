5aea10ce01b57ce2a2046a372f19710339fef2f6
HDTC <chienqh111@gmail.com>
2024-11-29 10:44:29 +0700

    them chuc nang dang nhap bang gg va quen mat khau

---
+++ ViewModels/Account/ResetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace YourProject.ViewModels.Account
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Mật khẩu phải ít nhất {2} ký tự.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Mật khẩu không khớp.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
