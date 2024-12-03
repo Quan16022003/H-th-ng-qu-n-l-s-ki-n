06a90856c069c976b1ab59e07c57d153168a6ae3
Nguyen Hoang Quan <nhq16022003@gmail.com>
2024-12-03 11:29:23 +0700

    sửa lỗi

--- EventManagementSystem/Controllers/OrderController.cs
+++ EventManagementSystem/Controllers/OrderController.cs
@@ -4,33 +4,8 @@ using Services.Abtractions;
 
 namespace Web.Controllers
 {
-    [ApiController]
-    [Route("/api[controller")]
     public class OrderController : ControllerBase
     {   
-        private IOrdersService _ordersService;
-        public OrderController(IOrdersService ordersService) {
-            _ordersService = ordersService;
-        }
-        [HttpPost("create")]
-        public async Task<IActionResult> CreateOrderAsync([FromBody] OrderDTO orderDTO)
-        {
-            if (orderDTO == null)
-            {
-                return BadRequest("OrderDTO cannot be null.");
-            }
-
-            try
-            {
-                // Gọi service để tạo đơn hàng
-                var orderId = await _ordersService.CreateOrderAsync(orderDTO);
-
-                return Ok(new { OrderId = orderId });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
+        
     }
 }
