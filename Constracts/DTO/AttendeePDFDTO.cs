2f0e435dd34bd8fda59452244b0a234683aed256
Hai129148 <kagami129148@gmail.com>
2024-12-13 20:20:39 +0700

    Add PDFService

---
+++ Constracts/DTO/AttendeePDFDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constracts.DTO
+{
+    public class AttendeePDFDTO : BaseDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int OrderId { get; set; }
+        public int TicketId { get; set; }
+        public int EventId { get; set; }
+    }
+}
