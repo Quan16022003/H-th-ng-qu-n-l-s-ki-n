226fcf5a912929c44a18b57d35a44bf8e20b5d18
Nguyen Hoang Quan <nhq16022003@gmail.com>
2024-12-14 10:59:16 +0700

    update

--- Domain/Repositories/IUnitOfWork.cs
+++ Domain/Repositories/IUnitOfWork.cs
@@ -24,6 +24,7 @@ namespace Domain.Repositories
         IOrderItemRepository OrderItemRepository { get; }
         IOrderTicketRepository OrderTicketRepository { get; }
         IAttendeeRepository AttendeeRepository { get; }
+       
 
         #endregion
 
